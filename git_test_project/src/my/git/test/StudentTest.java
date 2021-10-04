8eba9da388fcf651423f0604d75800c711934ab4
sinup <sinup@DESKTOP-RH6APPV>
2021-10-04 16:36:37 +0900

    Student 한명 더 추가

--- git_test_project/src/my/git/test/StudentTest.java
+++ git_test_project/src/my/git/test/StudentTest.java
@@ -4,5 +4,8 @@ public class StudentTest {
 	public static void main(String[] args) {
 		Student st = new Student("하하", 40);
 		System.out.println(st+"!!!!!!!");
+		
+		Student st1 =new Student("하하", 40);
+		System.out.println(st1+"!!!!!!!");
 	}
 }
